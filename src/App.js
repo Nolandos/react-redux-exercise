ca3fdf15c6eb27074d001fa620e73da7e95ae766
Łukasz Nolka <nolkalukasz@gmail.com>
2019-06-28 18:47:12 +0200

    Add redux thunk

--- src/App.js
+++ src/App.js
@@ -9,8 +9,18 @@ function search(users) {
   }
 }
 
-class App extends React.Component {
+function getUsers () {
+  return dispatch => {
+    const searchText = 'Nolandos'; // tak na sztywno sobie można podać :)
+    const url = `https://api.github.com/search/users?q=${searchText}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(responseJson => dispatch(search(responseJson.items)));
+  }
+}
 
+class App extends React.Component {
+  /*
   getUsers = () => {
     const searchText = 'Nolandos'; // tak na sztywno sobie można podać :)
     const url = `https://api.github.com/search/users?q=${searchText}`;
@@ -18,12 +28,13 @@ class App extends React.Component {
       .then(response => response.json())
       .then(responseJson => this.props.searchUsers(responseJson.items));
   }
+*/
 
 render() {
-  const { users } = this.props;
+  const { users, searchUsers } = this.props;
     return (
       <div>
-        <button onClick={this.getUsers} >Pokaż użytkowników</button>
+        <button onClick={searchUsers} >Pokaż użytkowników</button>
         <ul>{users.map(user => <User key={user.login} {...user} />)}</ul>
       </div>
     );
@@ -38,7 +49,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    searchUsers: (users) => dispatch(search(users))
+    searchUsers: () => dispatch(getUsers())
   }
 };
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 
 import App from './App';
@@ -18,7 +19,13 @@ const initialState = {
     users: []
 }
 
-const store = createStore(reducer, initialState);
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducer, 
+  initialState,
+  composeEnhancer(applyMiddleware(thunk)),
+  );
 
 
 render(
